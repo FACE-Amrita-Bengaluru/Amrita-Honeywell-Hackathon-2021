a28ab952953a959c6ba4a87dfcc8f89a2b503b7b
Harish <48942935+Subaharish@users.noreply.github.com>
2020-10-25 01:36:00 +0530

    Added New Changes (#52)
    * Added track icons
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Changed Alt tag
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added space between icons
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Rezied cybersecurity icon
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Change of dates
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * updated track content
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added title for each track
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added loader
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Changed month
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Fixed minor bugs
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Fixed bugs
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added Devfolio icon
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added icon of matic and portis
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added buttons for discord and brochure
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added Brochure link
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Fixed code factor issues
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Refactor EJS
    
    * Corrected spelling
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Aligned tracks in center
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Refactor
    
    * Added two more icons
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added Registration button
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Resolved merging conflicts
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Refactor index.ejs
    
    * Update main.css
    
    Co-authored-by: Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>

--- public/Main/js/main.js
+++ public/Main/js/main.js
@@ -136,3 +136,24 @@ $(document).ready(function () {
 		});
 	}
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    let devfolioOptions = {
+        buttonSelector: '#devfolio-apply-now',
+        key: 'slac2020',
+    }
+
+    let script = document.createElement('script');
+    script.src = "https://apply.devfolio.co";
+    document.head.append(script);
+
+    script.onload = function () {
+        new Devfolio(devfolioOptions);
+    }
+
+    script.onerror = function () {
+        document.querySelector(devfolioOptions.buttonSelector).addEventListener('click', function () {
+            window.location.href = 'https://slac2020.devfolio.co/';
+        });
+    }
+});
