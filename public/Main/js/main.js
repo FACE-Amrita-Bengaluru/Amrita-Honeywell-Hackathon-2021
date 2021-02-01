dbdf8006c9e5a1860d5ecdae64f77bd47035d809
Harish <48942935+Subaharish@users.noreply.github.com>
2021-02-01 13:21:14 +0530

    Final Changes (#3)
    * Started the countdown
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Replaced SLAC with Amrita Honeywell
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Removed community partners section
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added one stop guid button
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * added honeywell logo
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * added url to one stop guide
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * added amrita honeywell logo on header and footer
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added problem statement
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Added appropriate links to buttons
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Replaced amrita honeywell to amrita honeywell hackathon
    
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>
    
    * Update functions/views/index.ejs
    
    * Update functions/views/index.ejs
    
    * Update functions/views/index.ejs
    
    Co-authored-by: Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>

--- public/Main/js/main.js
+++ public/Main/js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 		}
 	});
 	$(".counter").counterUp({ delay: 5, time: 3000 });
-	$(".countdown").countdown("2020/11/07", function (event) {
+	$(".countdown").countdown("2021/02/10", function (event) {
 		$(this).html(
 			event.strftime(
 				"<div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>"
